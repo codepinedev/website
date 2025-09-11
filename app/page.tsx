295a27d9f47f0e2db1a1d47e15661237dcc97060
MhemedAbderrahmen <mhemedabderrahmen@gmail.com>
2025-09-11 16:56:34 +0100

    fix: favico and metadata + ui scaffold

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,21 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div>Hello Code Pine</div>
-        <Button>Click me</Button>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <div className="text-8xl font-bold text-center flex gap-4">
+          <Image src="/pine.png" alt="Pine icon" width={100} height={100} />
+          Code Pine
+        </div>
+        <div className="text-2xl text-center">
+          Sharpen your coding skills and stay cozy!
+        </div>
+        <div className="flex justify-center">
+          <Button size="lg">Work In Progress</Button>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -23,37 +30,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Learn (Under Construction)
         </a>
       </footer>
     </div>
