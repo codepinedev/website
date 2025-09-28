a3e4dc8a914c7ae7f19a9d6d078209cac0aa73bd
MhemedAbderrahmen <mhemedabderrahmen@gmail.com>
2025-09-28 14:06:44 +0100

    fix: join us header update

--- app/components/headers/header.tsx
+++ app/components/headers/header.tsx
@@ -8,7 +8,7 @@ import HeaderLink from "./header-link";
 export function Header() {
   return (
     <div className="flex flex-row justify-center sticky top-0 z-50">
-      <div className="max-w-4xl w-full flex flex-row gap-2 justify-between p-2 m-2 rounded-2xl  bg-background/50 backdrop-blur-sm shadow-sm">
+      <div className="max-w-4xl w-full flex flex-row gap-2 justify-between p-4 m-2 rounded-2xl  bg-background/50 backdrop-blur-sm shadow-sm">
         <Link href="/">
           <Image src="/pine.png" alt="Pine icon" width={40} height={40} />
         </Link>
