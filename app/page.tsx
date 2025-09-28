6cc2cbfa8d2511eefde452090ff249caee30f993
MhemedAbderrahmen <mhemedabderrahmen@gmail.com>
2025-09-28 12:41:02 +0100

    fix: hero section

--- app/page.tsx
+++ app/page.tsx
@@ -8,27 +8,38 @@ import { Socials } from "./components/socials";
 export default function Home() {
   return (
     <main className="flex flex-col gap-4 max-w-4xl w-full">
-      <div className="flex justify-center">
-        <Image src="/codepine-logo.png" alt="Pine icon" width={250} height={250} />
-      </div>
+      <section className="flex flex-col gap-4">
+        <div className="flex justify-center">
+          <Image src="/codepine-logo.png" alt="Pine icon" width={250} height={250} />
+        </div>
 
-      <div className="flex justify-center">
         <Badge variant="outline">🌲 Welcome to the Codepine Ecosystem</Badge>
-      </div>
+        <div>
+          <h1 className="text-5xl md:text-6xl font-bold text-foreground mb-6">
+            A
+            {" "}
+            <span className="text-primary">Cozy</span>
+            {" "}
+            Developer
+            <br />
+            <span className="text-primary">Ecosystem</span>
+          </h1>
+          {" "}
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            Codepine is an open, interactive and engaging community where any coder, from beginner to expert, can learn and grow together. Whether it's a tutorial, Q&A session, algorithmic problem solving or full application design and build, there's always something new to learn!
+          </p>
+        </div>
+        <div className="flex">
+          <Button size="lg">Connect With Pine Community</Button>
+        </div>
 
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">
-        CODEPINE
-      </h1>
-      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-        Sharpen your coding skills and stay cozy!
-      </h4>
-      <p className="leading-7 [&:not(:first-child)]:mt-6 text-justify">
-        Codepine is an open, interactive and engaging community where any coder, from beginner to expert, can learn and grow together. Whether it's a tutorial, Q&A session, algorithmic problem solving or full application design and build, there's always something new to learn!
-      </p>
-      <div className="flex">
-        <Button size="lg">Connect With Pine Community</Button>
-      </div>
-      <Socials />
+        <Socials />
+      </section>
+      <section className="flex flex-col gap-4">
+        <div className="flex justify-center">
+          <Image src="/hero.jpeg" alt="Codepine logo" width={600} height={600} className="rounded-lg" />
+        </div>
+      </section>
     </main>
   );
 }
